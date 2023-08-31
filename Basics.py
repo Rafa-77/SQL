db7655a9273ddbc9b3ba627aa7ba5f788e885502
Rafa-77 <alex_brandi77@hotmail.com>
2023-08-31 15:36:55 -0600

    modified:   Basics.py 	new file:   Images/Atributo Empleado.png 	new file:   Images/Atributo Persona.png 	new file:   Readme.md

---
+++ Basics.py
@@ -0,0 +1,7 @@
+# Entidad: Representacion de algo, ej: Casa
+# Atributos: Tamaño, precio, ventanas
+#   Atributos Simples: Precio, tamaño (representados por 1 ovalo)
+#   Atributos compuestos: Ambiente
+#   Atributos multivalor: Puerta, ventana (representados por 2 ovalos)
+#   Atributos derivados: antiguedad, ubicacion, se pueden obtener de otra informacion (representados por un ovalo con borde punteado)
+#   Atributo Key: Un identificador Unico, ID Vivienda (identificado con un ovalo y texto subrayado)
